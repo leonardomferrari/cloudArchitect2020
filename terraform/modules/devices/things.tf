6fb241c781832469295d6e10e8e05f17da050833
pabloperalta <pablojulianperalta@gmail.com>
2020-08-16 16:03:11 -0300

    Vamos a usar el mismo cert para todas las things porque sino tendria que levantar los ec2 uno por uno o tenes un mecanismo centralizado de asignacion de ids a los devices y no hay tiempo para eso. Queda el tema del count en el git de todas maneras.

--- terraform/modules/devices/things.tf
+++ terraform/modules/devices/things.tf
@@ -14,62 +14,50 @@ resource "aws_iot_thing_type" "pp-type" {
 }
 
 resource "aws_iot_certificate" "cert" {
-  count = 3
   active = true
 }
 
-resource "aws_s3_bucket_object" "s3_certs_folders" {
-  count = 3
+resource "aws_s3_bucket_object" "s3_certs_folder" {
   bucket = "aiouti-lf"
-  key    = "certs-${count.index}/"
+  key    = "certs/"
 }
 
 resource "local_file" "iot_cert_pem" {
-  count = 3
-  sensitive_content = aws_iot_certificate.cert[count.index].certificate_pem
-  filename = "${path.module}/devices/certs/cert-${count.index}/certificate.cert.pem"
+  sensitive_content = aws_iot_certificate.cert.certificate_pem
+  filename = "${path.module}/devices/certs/certificate.cert.pem"
 }
 
 resource "aws_s3_bucket_object" "s3_iot_cert_pem" {
-  count = 3
   bucket = "aiouti-lf"
-  key    = "certs-${count.index}/certificate.cert.pem"
-  source = "${path.module}/devices/certs/cert-${count.index}/certificate.cert.pem"
-  etag   = fileexists(local_file.cert_public_key[count.index].filename) ? filemd5(local_file.iot_cert_pem[count.index].filename) : md5("None")
+  key    = "certs/certificate.cert.pem"
+  source = local_file.iot_cert_pem.filename
 }
 
 resource "local_file" "cert_public_key" {
-  count = 3
-  sensitive_content = aws_iot_certificate.cert[count.index].public_key
-  filename = "${path.module}/devices/certs/cert-${count.index}/certificate.public.key"
+  sensitive_content = aws_iot_certificate.cert.public_key
+  filename = "${path.module}/devices/certs/certificate.public.key"
 }
 
 resource "aws_s3_bucket_object" "s3_cert_public_key" {
-  count = 3
   bucket = "aiouti-lf"
-  key    = "certs-${count.index}/certificate.public.key"
-  source = "${path.module}/devices/certs/cert-${count.index}/certificate.public.key"
-  etag   = fileexists(local_file.cert_public_key[count.index].filename) ? filemd5(local_file.cert_public_key[count.index].filename) : md5("None")
+  key    = "certs/certificate.public.key"
+  source = local_file.cert_public_key.filename
 }
 
 resource "local_file" "cert_private_key" {
-  count = 3
-  sensitive_content = aws_iot_certificate.cert[count.index].private_key
-  filename = "${path.module}/devices/certs/cert-${count.index}/certificate.private.key"
+  sensitive_content = aws_iot_certificate.cert.private_key
+  filename = "${path.module}/devices/certs/certificate.private.key"
 }
 
 resource "aws_s3_bucket_object" "s3_cert_private_key" {
-  count = 3
   bucket = "aiouti-lf"
-  key    = "certs-${count.index}/certificate.private.key"
-  source = "${path.module}/devices/certs/cert-${count.index}/certificate.private.key"
-  etag   = fileexists(local_file.cert_public_key[count.index].filename) ? filemd5(local_file.cert_private_key[count.index].filename) : md5("None")
+  key    = "certs/certificate.private.key"
+  source = local_file.cert_private_key.filename
 }
 
 resource "aws_iot_policy_attachment" "att-policy" {
-  count = 3
   policy = aws_iot_policy.iot_policy.name
-  target = aws_iot_certificate.cert[count.index].arn
+  target = aws_iot_certificate.cert.arn
 }
 
 resource "aws_iot_thing" "thing" {
@@ -84,7 +72,7 @@ resource "aws_iot_thing" "thing" {
 
 resource "aws_iot_thing_principal_attachment" "att-thing" {
   count = 3
-  principal = aws_iot_certificate.cert[count.index].arn
+  principal = aws_iot_certificate.cert.arn
   thing = aws_iot_thing.thing[count.index].name
 }
 
