2f4871d143ace17494feef99a3aaeaf436d2d48d
pabloperalta <pablojulianperalta@gmail.com>
2020-08-17 20:32:11 -0300

    Cambios al role del instance profile para que pueda acceder a S3 y tomar los certs. Cambios al user_data por lo mismo.

--- terraform/modules/devices/ecs-instance-profile/main.tf
+++ terraform/modules/devices/ecs-instance-profile/main.tf
@@ -18,9 +18,68 @@ resource "aws_iam_role" "this" {
 EOF
 }
 
+resource "aws_iam_policy" "aiouti-device-certs" {
+  name        = "aiouti-certs-policy"
+  path        = "/"
+  description = "Provides de test devices access to the certs"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "s3:GetObject",
+                "s3:PutObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::aiouti-lf/certs*"
+            ]
+        }
+    ]
+}
+EOF
+}
+
+resource "aws_iam_policy" "aiouti-device-log" {
+  name        = "aiouti-device-log"
+  path        = "/"
+  description = "Provides access to login functionality"
+
+  policy = <<EOF
+{
+    "Version": "2012-10-17",
+    "Statement": {
+        "Effect": "Allow",
+        "Action": [
+            "logs:CreateLogGroup",
+            "logs:CreateLogStream",
+            "logs:DescribeLogGroups",
+            "logs:DescribeLogStreams",
+            "logs:PutLogEvents",
+            "logs:GetLogEvents",
+            "logs:FilterLogEvents"
+        ],
+        "Resource": "arn:aws:logs:*:*:*"
+    }
+}
+EOF
+}
+
 resource "aws_iam_instance_profile" "this" {
   name = "${var.name}_ecs_instance_profile"
   role = aws_iam_role.this.name
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_device_certs" {
+  role       = aws_iam_role.this.id
+  policy_arn = aws_iam_policy.aiouti-device-certs.arn
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_device_log" {
+  role       = aws_iam_role.this.id
+  policy_arn = aws_iam_policy.aiouti-device-log.arn
 }
 
 resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
--- terraform/modules/devices/templates/user-data.sh
+++ terraform/modules/devices/templates/user-data.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-mkdir ~/cert
-aws s3 cp s3://aiouti-lf/certs/certificate.cert.pem ~/cert/certificate.cert.pem
-aws s3 cp s3://aiouti-lf/certs/certificate.public.key ~/cert/certificate.public.key
-aws s3 cp s3://aiouti-lf/certs/certificate.private.key ~/cert/certificate.private.key
-wget https://www.amazontrust.com/repository/AmazonRootCA1.pem -o ~/cert/certificate.root.pem
+cd /home
+cd ec2-user
+mkdir cert
+cd cert
+aws s3 cp s3://aiouti-lf/certs/certificate.cert.pem certificate.cert.pem
+aws s3 cp s3://aiouti-lf/certs/certificate.public.key certificate.public.key
+aws s3 cp s3://aiouti-lf/certs/certificate.private.key certificate.private.key
+wget https://www.amazontrust.com/repository/AmazonRootCA1.pem -o certificate.root.pem
 
 # ECS config
 {
