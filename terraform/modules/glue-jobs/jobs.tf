5bc137a1cc5a90de0a50aaacb4beb846f663946b
leonardomferrari <leonardomferrari@gmail.com>
2020-08-20 23:10:39 -0300

    delete de locale__file

--- terraform/modules/glue-jobs/jobs.tf
+++ terraform/modules/glue-jobs/jobs.tf
@@ -1,16 +1,8 @@
-locals {
-  jobpath = "${path.module}/tmp/jobs"
-}
 
-
-resource "local_file" "my_glue_job" {
-  filename = "${local.jobpath}/aiouti-job.py"
-}
 
 resource "aws_s3_bucket_object" "s3_my_glue_job_py" {
   bucket = "aiouti-lf"
   key    =  "lakeformation/measurements/job/aiouti"
   content = template_file.jobs_data.rendered
   etag = md5(template_file.jobs_data.rendered)
-  //source = local_file.my_glue_job.filename
 }
